0a759a890876cb6641c623069194d03984f5e826
Tony <heytonyyy3@gmail.com>
2025-05-27 06:37:34 -0700

    add TestFinch.java

---
+++ src/main/java/com/heytonyy/FinchTest.java
@@ -0,0 +1,14 @@
+package com.heytonyy;
+
+import com.birdbrain.Finch;
+
+public class FinchTest {
+    public static void main(String[] args) {
+        Finch f = new Finch();
+
+        // TODO: Practice using the Finch!
+
+        f.stop();
+        f.disconnect();
+    }
+}
