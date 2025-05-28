b89e1e4d834f07d7c79c1dca3867d19080682a14
imranrasheedsc <107059911+imranrasheedsc@users.noreply.github.com>
2025-05-28 14:19:29 -0700

    yes

---
+++ src/main/java/com/Imrangroup/Test.java
@@ -0,0 +1,13 @@
+package com.imranrasheedsc;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        int scores = new int[20];
+
+        for (int i : scores) {
+            System.out.println(i);
+        }
+    }
+}
