df51d89d4f97a5363dce236b687a8fc5abf254c6
imranrasheedsc <107059911+imranrasheedsc@users.noreply.github.com>
2025-05-30 14:26:25 -0700

    yes

--- src/main/java/com/imranrasheed/Test.java
+++ src/main/java/com/imranrasheed/Test.java
@@ -1,18 +1,50 @@
-package com.example;
+package com.imranrasheed;
 
 import com.birdbrain.Finch;
 
+
 public class FinchDemo {
+    public static boolean isBlocked(Finch object) {
+        if (object.getDistance() < 8) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         Finch myFinch = new Finch();
 
+        boolean enabled = true;
+    
+
         // myFinch.playNote(60,0.5);
 
-        for (int i = 0; i < 10; i++) {
-            myFinch.setBeak(100, 100, 100);
-            myFinch.pause(1);
-            myFinch.setBeak(0, 0, 0);
-            myFinch.pause(1);
+        while (enabled) {
+            myFinch.setMove("F",10,100);
+            System.out.println(myFinch.getDistance());
+            while (myFinch.getDistance() < 8) {
+                myFinch.stop();
+                enabled = false;
+                System.out.println(myFinch.getCompass());
+                while (FinchDemo.isBlocked(myFinch)) {
+                    if (myFinch.getCompass() > 90) {
+                        myFinch.setTurn("L",90,100);
+                    } else {
+                        myFinch.setTurn("R",180,100);
+                    }
+                }
+                myFinch.pause(0.01);
+                enabled = true;
+            }
+            // if (myFinch.getDistance() < 7) {
+            //     myFinch.stop();
+            //     enabled = false;
+            //     myFinch.setTurn("R",90,100);
+            //     myFinch.pause(0.05);
+            //     System.out.println(myFinch.getDistance());
+            //     enabled = true;
+            // }
         }
 
         myFinch.stopAll();
