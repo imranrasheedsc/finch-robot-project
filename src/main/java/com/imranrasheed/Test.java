e762dbf95126b6e74f65cce481c89a5c258207ec
imranrasheedsc <107059911+imranrasheedsc@users.noreply.github.com>
2025-06-02 14:22:54 -0700

    yes

--- src/main/java/com/imranrasheed/Test.java
+++ src/main/java/com/imranrasheed/Test.java
@@ -4,13 +4,13 @@ import com.birdbrain.Finch;
 
 
 public class FinchDemo {
-    public static boolean isBlocked(Finch object) {
-        if (object.getDistance() < 8) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // public static boolean isBlocked(Finch object) {
+    //     if (object.getDistance() < 8) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 
     public static void main(String[] args) {
         Finch myFinch = new Finch();
@@ -22,22 +22,41 @@ public class FinchDemo {
         System.out.println("yes");
 
         while (enabled) {
-            myFinch.setMove("F",10,100);
-            System.out.println(myFinch.getDistance());
-            while (myFinch.getDistance() < 8) {
-                myFinch.stop();
-                enabled = false;
-                System.out.println(myFinch.getCompass());
-                while (FinchDemo.isBlocked(myFinch)) {
-                    if (myFinch.getCompass() > 90) {
-                        myFinch.setTurn("L",90,100);
-                    } else {
-                        myFinch.setTurn("R",180,100);
-                    }
-                }
-                myFinch.pause(0.01);
-                enabled = true;
+            if (math.abs(myFinch.getCompass() - 90) < math.abs(myFinch.getCompass() - 180)) {
+                myFinch.setTurn()
             }
+
+            System.out.println("Direction facing: " + myFinch.getCompass());
+            while (myFinch.getDistance() > 15) {
+                myFinch.setBeak(0,255,0);
+                myFinch.setMove("F",20,100);
+                System.out.println("Right line sensor: " + myFinch.getLine("R"));
+                // if (myFinch.getLine("R") > 90) {
+                //     myFinch.setTurn("L",-myFinch.getCompass(),50);
+                //     myFinch.setTurn("L",90,50);
+                //     myFinch.stop();
+                //     enabled = false;
+                // }
+                // myFinch.stop();
+                // enabled = false;
+                // System.out.println(myFinch.getCompass());
+                // while (FinchDemo.isBlocked(myFinch)) {
+                //     if (myFinch.getCompass() > 90) {
+                //         myFinch.setTurn("L",90,100);
+                //     } else {
+                //         myFinch.setTurn("R",180,100);
+                //     }
+                // }
+                // myFinch.pause(0.01);
+                // enabled = true;
+            }
+            myFinch.setBeak(255,0,0);
+            myFinch.stop();
+            myFinch.setTurn("L",90,50);
+            if (myFinch.getDistance() < 15) {
+                myFinch.setTurn("L",180,50);
+            }
+
             // if (myFinch.getDistance() < 7) {
             //     myFinch.stop();
             //     enabled = false;
